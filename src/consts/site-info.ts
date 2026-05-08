247b8f452cc4ee327fffe170a11cc689a8e5dfc7
ssamucr <ssamucr@gmail.com>
2026-05-07 22:30:07 -0500

    Setup the MainLayout object and NavBar

---
+++ src/consts/site-info.ts
@@ -0,0 +1,4 @@
+export const SITE_INFO = {
+    title: 'Samuel\'s Portfolio',
+    description: 'Welcome to my portfolio! Here you can find information about me, my projects, and how to contact me.',
+}
---
+++ tailwind.config.cjs
@@ -0,0 +1,11 @@
+module.exports = {
+  content: ['./src/**/*.{astro,html,js,ts,jsx,tsx}'],
+  theme: {
+    extend: {
+      colors: {
+        'samu-dark': '#0E0E0E',
+      },
+    },
+  },
+  plugins: [],
+};
