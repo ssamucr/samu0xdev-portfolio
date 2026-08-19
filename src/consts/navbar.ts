be8940a3fa474aea1e5b373076cd072676a0581b
ssamucr <ssamucr@gmail.com>
2026-08-18 21:55:03 -0500

    feat: comment out ContactSection in index page and navbar, add selection styles

--- src/consts/navbar.ts
+++ src/consts/navbar.ts
@@ -3,5 +3,5 @@ export const navItems = [
     { label: "Projects",   href: "#projects-section",   active: false },
     { label: "About",      href: "#about-section",      active: false },
     { label: "Experience", href: "#experience-section", active: false },
-    { label: "Contact",    href: "#contact-section",    active: false },
+    // { label: "Contact",    href: "#contact-section",    active: false },
 ];
